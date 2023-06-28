0cb7e1b8ef82263cb021ce19c95ac8f72cb2a867
Levy-Naibei <levynaibei2@gmail.com>
2023-06-28 20:57:19 +0300

    chore: WIP

--- src/js/main/ThemeGenerator.tsx
+++ src/js/main/ThemeGenerator.tsx
@@ -1,129 +1,173 @@
-import React, {useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import './theme.css'
 
+interface Theme {
+  colors: string[];
+  style: string;
+}
+
 const ThemeGenerator: React.FC = () => {
-    const [colorPalette, setColorPalette] = useState<string[]>([]);
-    const [designStyle, setDesignStyle] = useState<string>("");
-    const [savedThemes, setSavedThemes] = useState<string[]>([]);
-    // const [colorSelected, setSelectedColor] = useState<string>("");
-  
-    useEffect(() => {
-      loadSavedThemes();
-    }, [])
-  
-    const getRandomHexColor = () => {
-      let hexColor = '#';
-      const baseHexColorCode = '0123456789ABCDEF';
-      for (let i = 0; i < 6; i++) {
-        hexColor += baseHexColorCode[Math.floor(Math.random() * 16)];
-      }
-      return hexColor;
+  const [designStyle, setDesignStyle] = useState<string>('');
+  const [colorPalette, setColorPalette] = useState<string[]>([]);
+  const [savedThemes, setSavedThemes] = useState<Theme[]>([]);
+
+  const generateColorPalette = () => {
+    let colors = [];
+    for (let i = 0; i < 5; i++) {
+      let randomColor = getRandomHexColor();
+      colors.push(randomColor);
     }
-  
-    const generateColorPalette = () => {
-      let colors = [];
-      for (let i = 0; i < 5; i++) {
-        let randomColor = getRandomHexColor();
-        colors.push(randomColor);
-      }
-      setColorPalette(colors);
-    }
-  
-    const generateDesign = () => {
-      const designStyles = ["vintage", "futuristic", "minimalistic"];
-      const n = designStyles.length;
-      let randomDesign = designStyles[Math.round(Math.random() * n)];
-      setDesignStyle(randomDesign);
-    }
-  
-    const saveThemes = () => {
-      try {
-        const data = localStorage.getItem("savedThemes");
-        console.log("data", data);
-        const saveThemes = data ? JSON.parse(data) : [];
-        const newTheme = { colorPalette, designStyle };
-        const updatedThemes = [...saveThemes, newTheme];
-        localStorage.setItem("savedThemes", JSON.stringify(updatedThemes));
-        setSavedThemes(updatedThemes);
-      } catch (error) {
-        console.log("Error occured!");
-      }
-    }
-  
-    console.log("saved items ", savedThemes);
-  
-    const loadSavedThemes = () => {
-      const savedThemesData = localStorage.getItem('savedThemes');
-      const savedThemes = savedThemesData ? JSON.parse(savedThemesData) : [];
-      setSavedThemes(savedThemes);
+    setColorPalette(colors);
+  };
+
+  const generateDesignStyle = () => {
+    const designStyles = ['Vintage', 'Futuristic', 'Minimalistic', 'Gringe', 'Abstract'];
+    const randomStyle = designStyles[Math.floor(Math.random() * designStyles.length)];
+    setDesignStyle(randomStyle);
+  };
+
+  const saveTheme = () => {
+    const theme: Theme = {
+      colors: [...colorPalette],
+      style: designStyle,
     };
-  
-    const clearSavedThemes = () => {
-      localStorage.removeItem('savedThemes');
-      setSavedThemes([]);
-    };
-  
-    return (
-      <div className="container">
-        <div className="theme_container">
-          <button onClick={generateColorPalette}>Generate Pallete</button>
-          <div className="color_palette">
-            {colorPalette.map((color, index) => {
-              return (
-                <div
-                  // onMouseDown={selectedColor(color)}
-                  className="palette"
-                  style={{ backgroundColor: `${color}` }}
-                  key={index}
-                >
-                </div>
-              )
-            })}
+    setSavedThemes([...savedThemes, theme]);
+    setColorPalette([]);
+    setDesignStyle('');
+  };
+
+  const clearSavedThemes = () => {
+    setSavedThemes([]);
+  };
+
+  const getRandomHexColor = () => {
+    let hexColor = '#';
+    const baseHexColorCode = '0123456789ABCDEF';
+    for (let i = 0; i < 6; i++) {
+      hexColor += baseHexColorCode[Math.floor(Math.random() * 16)];
+    }
+    return hexColor;
+  }
+
+  return (
+    <div className="App"
+    // style={{backgroundColor: "grey"}}
+    >
+      <button className="custom-btn" onClick={generateColorPalette}>Generate Palette</button>
+      <button className="custom-btn" onClick={generateDesignStyle}>Generate Style</button>
+      <button className="custom-btn" onClick={saveTheme}>Save Theme</button>
+      <button className="custom-btn" onClick={clearSavedThemes}>Clear Saved</button>
+
+      <div className="color-palette">
+        {colorPalette.map((color: string, index: number) => (
+          <div
+            key={index}
+            style={{ backgroundColor: color }}
+            className="color-box">
           </div>
-        </div>
-  
-        <div className="theme_container">
-          <button onClick={generateDesign}>Generate Style</button>
-          <div className='design_style'>
-            {designStyle}
-          </div>
-        </div>
-  
-        <div className="theme_container">
-          <button onClick={saveThemes}>Save Theme</button>
-          <div>
-            {savedThemes.map((theme, index) => (
-              (
-                // <>
-                <div key={index}>
-                  <div>Color Palette</div>
-                  <div className="color_palette">
-                    {colorPalette.map((color, index) => {
-                      return (
-                        <div
-                          className="palette"
-                          style={{ backgroundColor: `${color}` }}
-                          key={index}
-                        >
-                        </div>
-                      )
-                    })}
-                  </div>
-                  <div>
-                    <div>Design Style</div>
-                    {designStyle}
-                  </div>
-                </div>
-                // </>
-              )
-            ))}
-          </div>
-        </div>
-        <div className="theme_container">
-          <button onClick={clearSavedThemes}>Clear Saved</button>
+        ))}
+      </div>
+
+      <div className="custom_container">
+        <h4 className="app_sub_headers">Customize Theme</h4>
+        <input
+          className="custom_input"
+          type="color"
+          value={colorPalette[0] || ''}
+          onChange={(e) => setColorPalette(
+            [
+              e.target.value,
+              ...colorPalette.slice(1)
+            ]
+          )}
+        />
+
+        <input
+          className="custom_input"
+          type="color"
+          value={colorPalette[1] || ''}
+          onChange={(e) => setColorPalette(
+            [
+              colorPalette[0],
+              e.target.value,
+              ...colorPalette.slice(2)
+            ]
+          )}
+        />
+
+        <input
+          className="custom_input"
+          type="color"
+          value={colorPalette[2] || ''}
+          onChange={(e) => setColorPalette(
+            [
+              colorPalette[0],
+              colorPalette[1],
+              e.target.value,
+              ...colorPalette.slice(3)
+            ]
+          )}
+        />
+
+        <input
+          className="custom_input"
+          type="color" 
+          value={colorPalette[3] || ''}
+          onChange={(e) => setColorPalette(
+            [
+              colorPalette[0],
+              colorPalette[1],
+              colorPalette[2],
+              e.target.value,
+              ...colorPalette.slice(4)
+            ]
+          )}
+        />
+
+        <input
+          className="custom_input"
+          type="color"
+          value={colorPalette[4] || ''}
+          onChange={(e) => setColorPalette(
+            [
+              colorPalette[0],
+              colorPalette[1],
+              colorPalette[2],
+              colorPalette[3],
+              e.target.value
+            ]
+          )}
+        />
+
+        <div>
+          <input
+            type="text"
+            placeholder="design style"
+            id="custom-style"
+            value={designStyle}
+            onChange={(e) => setDesignStyle(e.target.value)}
+          />
         </div>
       </div>
-    )
+      <div className="saved_themes">
+        <h4 className="app_sub_headers">Your saved themes</h4>
+        {savedThemes.length ? savedThemes.map((theme: Theme, index: number) => (
+          <div key={index} className="saved_theme">
+            <div className="theme_colors">
+              {theme.colors.map((color: string, index: number) => (
+                <div
+                  key={index}
+                  style={{ backgroundColor: color }}
+                  className="color-box">
+                </div>
+              ))}
+            </div>
+            <div className="theme-style">{theme.style}</div>
+          </div>
+        )) : <p>No saved themes yet</p>}
+      </div>
+    </div>
+  );
 }
 
 export default ThemeGenerator;
--- src/js/main/main.tsx
+++ src/js/main/main.tsx
@@ -4,7 +4,6 @@ import './main.css';
 const Main = () => {
   return (
     <div className="app">
-      <h1 className="app-header">Random Theme Generator</h1>
       <ThemeGenerator />
     </div>
   );
